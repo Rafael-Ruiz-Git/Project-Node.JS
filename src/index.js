65ba06ab926af14b7735dfbc13d91a3233c450fb
Rafael Ruiz <ruiz2099alex@gmail.com>
2025-08-12 14:21:43 -0400

    Update

--- src/index.js
+++ src/index.js
@@ -1,11 +1,39 @@
 const express = require("express");
+const fs =require("fs");
+const path = require("path");
+const morgan = require("morgan");
 const tasksRouter = require("./tasks/tasks.router.js");
+const cors = require("cors");
 
 const app = express();
 const port = 3001; // 0 - 65,535 http://localhost:3001/ to restart is cntrl + c in console 
 
+app.use(express.json());
+
+const corsOptions = {
+  origin: ["example.com", "example2.com"]
+}
+
+app.use(cors());
+
+
+let accessLogStream = fs.createWriteStream(path.join(__dirname, "..","access.log"), {
+  flags: "a",
+});
+
+app.use(morgan("combined", {stream: accessLogStream}));
+
+const middleWare = function (req, res, next) {
+  req.info = {appname: "Task Manager", author: "Rafael Ruiz"};
+  next();
+};
+
+app.use(middleWare);
+
 /*Define routs */
 app.use("/", tasksRouter);
+
+
 
 
 app.listen(port, () => {
--- src/tasks/tasks.router.js
+++ src/tasks/tasks.router.js
@@ -4,8 +4,14 @@ const express = require("express");
 const tasksRouter= express.Router();
 
 
-tasksRouter.get("/tasks", (req,res)=> res.send("All Tasks"));
+tasksRouter.get("/tasks", (req,res)=> {
+    return res.send("Hello!!");
+});
 
-tasksRouter.post("/tasks", (req,res)=> res.send("Creating a new Task"));
+tasksRouter.post("/tasks", (req,res)=> {
+  console.log(req.body);
+  console.log(typeof req.body);
+  return res.send("Create new Taks");
+});
 
 module.exports = tasksRouter;
