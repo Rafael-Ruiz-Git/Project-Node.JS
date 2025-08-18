9416bb4e5c7f4cf623a4659bd5c58e1f797b707c
Rafael Ruiz <ruiz2099alex@gmail.com>
2025-08-18 11:15:02 -0400

    Update

--- src/middleware/responseFormatter.js
+++ src/middleware/responseFormatter.js
@@ -11,11 +11,23 @@ function responseFormatter(req, res, next) {
 
     const response = {
       status: statusCode >= 200 && statusCode < 300 ? "success" : "error",
-      statusCode: statusCode,
+      statusCode: res.statusCode,
       message: getReasonPhrase(res.statusCode),
-      data: statusCode >= 200 && statusCode < 300 ? data : null,
-      error: statusCode >= 200 && statusCode < 300 ? null : data,
+      // data: res.statusCode >= 200 && res.statusCode < 300 ? data : null,
+      // error: res.statusCode >= 200 && res.statusCode < 300 ? null : data,
     };
+
+    if(res.statusCode >= 200 && res.statusCode < 300) {
+      response.data = data.pagination ? data.data : data;
+    }
+
+    if(res.statusCode > 300 ){
+      response.error = data;
+    }
+
+    if(data.pagination){
+      response.pagination = data.pagination;
+    }
 
     originalJson.call(res, response);
   };
--- src/tasks/providers/getTasks.providers.js
+++ src/tasks/providers/getTasks.providers.js
@@ -6,12 +6,42 @@ const errorLogger = require("../../helpers/errorLogger.helper.js");
 async function getTasksProvider(req, res) {
   const data = matchedData(req);
 
-  console.log(data);
-  const tasks = await Task.find();
   try {
+    const totalTasks = await Task.countDocuments();
+    const currentPage = Number(data.page) || 1;
+    const limit = data.limit;
+    const order = data.order;
+    const totalPages = Math.ceil(totalTasks/ limit);
+    const nextPage = currentPage === totalPages ? currentPage : currentPage + 1;
+    const previousPage = currentPage === 1 ? currentPage : currentPage - 1;
+    const baseUrl = `${req.protocol}://${req.get("host")}${req.originalUrl.split("?")[0]}`;
 
-    const tasks = await Task.find();
-    return res.status(StatusCodes.OK).json(tasks);
+    const tasks = await Task.find({
+      status: {$in: ["todo", "inProgress"]},
+    }).limit(limit).skip(currentPage-1).sort({
+      createdAt: order === "asc" ? 1 : -1,
+    });
+
+let finalResponse = {
+  data: tasks,
+  pagination: {
+    meta: {
+    itemsPerPage: limit,
+    totalItems: totalTasks,
+    currentPage: currentPage,
+    totalPages: totalPages,
+  },
+  links: {
+    first: `${baseUrl}/?limit=${limit}&page=${1}&order=${order}`,
+    last: `${baseUrl}/?limit=${limit}&page=${totalPages}&order=${order}`,
+    currentPage: `${baseUrl}/?limit=${limit}&page=${currentPage}&order=${order}`,
+    next: `${baseUrl}/?limit=${limit}&page=${nextPage}&order=${order}`,
+    previous: `${baseUrl}/?limit=${limit}&page=${previousPage}&order=${order}`,
+  }
+    },
+};
+
+    return res.status(StatusCodes.OK).json(finalResponse);
   } catch (error) {
     errorLogger("Error while fetching task: ", req, error);
     return res.status(StatusCodes.GATEWAY_TIMEOUT).json({
